5c065b15af617b36a5c8625fa2273869d169e0ff
Ronit Panda <pandaronit25@gmail.com>
2022-10-22 17:38:57 +0530

    feat: added feature for users to send money to each other

--- src/utils/constants.util.ts
+++ src/utils/constants.util.ts
@@ -1,3 +1,4 @@
 export const __isProd__ = process.env.NODE_ENV === 'production';
-export const adminUserUuid = process.env.ADMIN_USER_ID || 'admin';
+export const adminUserUuid = process.env.ADMIN_USER_UUID || 'admin';
+export const adminUserId = '6353d66e78811f65d1f9d0b7';
 export const oneMinuteInMilliseconds = 60000;
